8e40409c3a7257042d64e77a2065cda49017e768
Dor256 <dorb@wix.com>
2021-08-03 19:33:50 +0300

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM openjdk:8
+
+ENV SBT_VERSION 1.5.5
+
+RUN \
+  echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | tee /etc/apt/sources.list.d/sbt.list && \
+  echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | tee /etc/apt/sources.list.d/sbt_old.list && \
+  curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add && \
+  apt-get update && \
+  apt-get install sbt
+
+WORKDIR /swole
+
+ADD . /swole
+
+CMD sbt run
--- src/main/scala/com/unpacked/swole/db/Seed.scala
+++ src/main/scala/com/unpacked/swole/db/Seed.scala
@@ -6,10 +6,15 @@ import scala.io.Source
 object Seed {
   implicit val session: AutoSession = AutoSession
 
+  def dangerousSql(statement: String): Boolean =
+    statement.startsWith("--") ||
+    statement.startsWith("/*") ||
+    statement.isEmpty
+
   def apply(): Unit = {
     val schema = Source.fromResource("schema.sql")
     val statements = schema.getLines()
-      .filterNot(line => line.startsWith("--") || line.startsWith("/*") || line.isEmpty)
+      .filterNot(dangerousSql)
       .mkString
       .split(";")
       .map(SQL(_).execute().apply())
